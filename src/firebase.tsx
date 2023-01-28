46812228bf87be9ef2305112e5741042ba034b82
Pham Minh Tan <tanphamminh2002@gmail.com>
2023-01-27 19:45:37 -0800

    sign in

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,14 +6,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-	apiKey: 'AIzaSyAcvjpcf0Ipjdb_xL_ZuOtLIoPwYLXp37c',
-	authDomain: 'fullstack-8f9b0.firebaseapp.com',
-	projectId: 'fullstack-8f9b0',
-	storageBucket: 'fullstack-8f9b0.appspot.com',
-	messagingSenderId: '295890992573',
-	appId: '1:295890992573:web:c022ac0d72546687ba26d4',
-	measurementId: 'G-NDH6MBJ4L0',
+const firebaseConfig = {
+	apiKey: 'AIzaSyAKyGdu_46lwlGoswkxS3sISQZFRxL8PNM',
+	authDomain: 'ecom-typescript-phamminhtan.firebaseapp.com',
+	projectId: 'ecom-typescript-phamminhtan',
+	storageBucket: 'ecom-typescript-phamminhtan.appspot.com',
+	messagingSenderId: '967904015533',
+	appId: '1:967904015533:web:5d1298085ebc2d6f32c224',
+	measurementId: 'G-XM4ZBNRXTR',
 };
 
 const firebaseApp: any = initializeApp(firebaseConfig);
